e337a43893b87909d45a7ab6ceeb085fbb5c261c
Praktikant <praktikantin@spedion.de>
2022-02-18 08:57:46 +0100

    bug fix

--- lib/Recorder.dart
+++ lib/Recorder.dart
@@ -13,6 +13,7 @@ class Recorder {
   double _longitude = 0;
   double _speed = 0;
   DateTime? _timestamp;
+  Track? track;
 
   StreamSubscription<Position>? positionStream;
 
@@ -20,7 +21,7 @@ class Recorder {
 
 
 
-  Future<void> _updatePosition(Track track) async {
+  Future<void> _updatePosition() async {
 
     bool serviceEnabled;
     LocationPermission permission;
@@ -52,23 +53,25 @@ class Recorder {
       _speed = pos.speed;
       _timestamp = pos.timestamp;
       print(_latitude.toString() + ' - ' + _longitude.toString() + ' - ' + _speed.toString());
-      track.startTime ??= pos.timestamp;
+      track?.startTime ??= pos.timestamp;
       GeoPosition newPos = GeoPosition.fromPosition(_latitude, _longitude, _speed, _timestamp);
-      track.positions.add(newPos);
-      track.endTime = pos.timestamp;
-      track.save();
+      track?.positions.add(newPos);
+      track?.endTime = pos.timestamp;
+      track?.save();
     });
   }
 
 
   void startRecording() {
     _isRecording = true;
-    final newTrack = Track();
-    Hive.box('tracks').add(newTrack);
-    _updatePosition(newTrack);
+    track = Track();
+    Hive.box('tracks').add(track);
+    _updatePosition();
   }
 
   Future<void> stopRecording() async {
+    track?.endTime = DateTime.now();
+    track?.save();
     positionStream?.cancel();
     _isRecording = false;
   }
