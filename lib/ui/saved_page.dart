01b2a6fd72974dc4b35a8a7018eef610c2d1adcd
Praktikant <praktikantin@spedion.de>
2022-02-28 08:03:18 +0100

    replace WachBoxBuilder with ValueListenableBuilder

--- lib/ui/saved_page.dart
+++ lib/ui/saved_page.dart
@@ -50,100 +50,100 @@ class _SavedPageState extends State<SavedPage>
 
   @override
   Widget build(BuildContext context) {
-    return WatchBoxBuilder(
-        box: Hive.box('tracks'),
-        builder: (context, tracksBox) {
+    return ValueListenableBuilder<Box>(
+        valueListenable: Hive.box('tracks').listenable(),
+        builder: (BuildContext context, Box tracks, Widget? child) {
           return ListView.builder(
             physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-            itemCount: tracksBox.length,
+            itemCount: tracks.length,
             itemBuilder: (context, index) {
-              final track = tracksBox.getAt(index) as Track;
+              final track = tracks.getAt(index) as Track;
               return Card(
                   child: Slidable(
-                closeOnScroll: true,
-                dragStartBehavior: DragStartBehavior.start,
-                endActionPane: ActionPane(
-                  extentRatio: .35,
-                  motion: const DrawerMotion(),
-                  children: [
-                    SlidableAction(
-                      onPressed: (context){_showAlertDialog(context, track);},
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
-                      icon: Icons.delete,
-                      label: 'Delete',
+                    closeOnScroll: true,
+                    dragStartBehavior: DragStartBehavior.start,
+                    endActionPane: ActionPane(
+                      extentRatio: .35,
+                      motion: const DrawerMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (context){_showAlertDialog(context, track);},
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      'Track from ' +
-                          (track.startTime != null
-                              ? ValueFormat()
+                    child: ListTile(
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          'Track from ' +
+                              (track.startTime != null
+                                  ? ValueFormat()
                                   .dateFormatter
                                   .format(track.startTime!.toLocal())
-                              : 'unknown'),
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  minVerticalPadding: 10,
-                  contentPadding: const EdgeInsets.only(
-                      left: 16, right: 16, top: 7, bottom: 7),
-                  subtitle: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
+                                  : 'unknown'),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      minVerticalPadding: 10,
+                      contentPadding: const EdgeInsets.only(
+                          left: 16, right: 16, top: 7, bottom: 7),
+                      subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          const Text('Start Time: ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text(track.startTime != null
-                              ? ValueFormat()
+                          Column(
+                            children: [
+                              const Text('Start Time: ',
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text(track.startTime != null
+                                  ? ValueFormat()
                                   .timeFormatter
                                   .format(track.startTime!.toLocal())
-                              : 'unknown'),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text('End Time: ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text(track.endTime != null
-                              ? ValueFormat()
+                                  : 'unknown'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              const Text('End Time: ',
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text(track.endTime != null
+                                  ? ValueFormat()
                                   .timeFormatter
                                   .format(track.endTime!.toLocal())
-                              : 'unknown'),
+                                  : 'unknown'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              const Text('Max. Speed: ',
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text(ValueFormat()
+                                  .formatAndCheckSpeedValue(track.maxSpeed)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              const Text('Avg. Speed: ',
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              Text(ValueFormat()
+                                  .formatAndCheckSpeedValue(track.avgSpeed)),
+                            ],
+                          ),
                         ],
                       ),
-                      Column(
-                        children: [
-                          const Text('Max. Speed: ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text(ValueFormat()
-                              .formatAndCheckSpeedValue(track.maxSpeed)),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text('Avg. Speed: ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          Text(ValueFormat()
-                              .formatAndCheckSpeedValue(track.avgSpeed)),
-                        ],
-                      ),
-                    ],
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => TrackDetailPage(track)));
-                  },
-                ),
-              ));
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TrackDetailPage(track)));
+                      },
+                    ),
+                  ));
             },
           );
-        });
+    });
   }
 }
